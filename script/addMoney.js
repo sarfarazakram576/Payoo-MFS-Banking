b165b2d15ace62040552b75ee4f7454a1c509d1f
sarfarazakram576 <sarfaraz.akram055@gmail.com>
2025-02-27 21:49:42 +0600

    add somethings

--- script/addMoney.js
+++ script/addMoney.js
@@ -10,43 +10,47 @@ document
       "bank-account-number"
     ).value;
     const mainBalance = getInnerTextById("mainBalance");
+    const selectedBank = document.getElementById("bankForAddMoney").value;
 
     if (bankAccountNumber.length === 11) {
       if (pin === 1234) {
-        if (amount > 0) {
-          const newMainBalance = mainBalance + amount;
-          setInnerTextByIdAndValue("mainBalance", newMainBalance);
+        if (selectedBank !== "Select Bank") {
+          if (amount > 0) {
+            const newMainBalance = mainBalance + amount;
+            setInnerTextByIdAndValue("mainBalance", newMainBalance);
 
-          const selectedBank = document.getElementById("bankForAddMoney").value;
-          const container = document.getElementById("transection-container");
-          const div = document.createElement("div");
-          div.classList.add(
-            "p-6",
-            "bg-white",
-            "mb-4",
-            "rounded-xl",
-            "border",
-            "border-gray-300"
-          );
-          div.innerHTML = `
-              
-      <div class="flex gap-3 items-center">
-        <div class="bg-[#F4F5F7] p-3 rounded-full">
-          <img src="assets/wallet1.png" alt="" class="w-10" />
+            const container = document.getElementById("transection-container");
+            const div = document.createElement("div");
+            div.classList.add(
+              "p-6",
+              "bg-white",
+              "mb-4",
+              "rounded-xl",
+              "border",
+              "border-gray-300"
+            );
+            div.innerHTML = `
+                
+        <div class="flex gap-3 items-center">
+          <div class="bg-[#F4F5F7] p-3 rounded-full">
+            <img src="assets/wallet1.png" alt="" class="w-10" />
+          </div>
+          <div>
+            <h3 class="font-bold text-lg text-[#525252]">Add Money</h3>
+            <p class="text-[#525252] font-semibold">${currentTime}</p>
+          </div>
         </div>
-        <div>
-          <h3 class="font-bold text-lg text-[#525252]">Add Money</h3>
-          <p class="text-[#525252] font-semibold">${currentTime}</p>
-        </div>
-      </div>
-      <p class="text-[#525252] font-semibold">You Added $${amount} to your account from this ${bankAccountNumber} account number of ${selectedBank} (bank or online banking app)</p>
-    
-          `;
-          container.appendChild(div);
-        } else if (amount === 0) {
-          alert("you can`t add $0");
+        <p class="text-[#525252] font-semibold">You Added $${amount} to your account from this ${bankAccountNumber} account number of ${selectedBank} (bank or online banking app)</p>
+      
+            `;
+            container.appendChild(div);
+          } else if (amount === 0) {
+            alert("you can`t add $0");
+          } else {
+            alert("you can`t add negative amount");
+          }
         } else {
-          alert("you can`t add negative amount");
+          alert("Select a bank");
         }
       } else {
         alert("pin thik nai");
--- script/payBill.js
+++ script/payBill.js
@@ -7,48 +7,51 @@ document
     const pin = getInputValueById("pay-bill-pin");
     const billerNumber = document.getElementById("biller-account-number").value;
     const mainBalance = getInnerTextById("mainBalance");
+    const selectedBank = document.getElementById("bankForPayBill").value;
 
     if (billerNumber.length === 11) {
       if (pin === 1234) {
-        if (amount <= mainBalance) {
-          if (amount > 0) {
-            const newMainBalance = mainBalance - amount;
-            setInnerTextByIdAndValue("mainBalance", newMainBalance);
+        if (selectedBank !== "Select Bank or Online Banking App") {
+          if (amount <= mainBalance) {
+            if (amount > 0) {
+              const newMainBalance = mainBalance - amount;
+              setInnerTextByIdAndValue("mainBalance", newMainBalance);
 
-            const selectedBank =
-              document.getElementById("bankForPayBill").value;
-            const container = document.getElementById("transection-container");
-            const div = document.createElement("div");
-            div.classList.add(
-              "p-6",
-              "bg-white",
-              "mb-4",
-              "rounded-xl",
-              "border",
-              "border-gray-300"
-            );
-            div.innerHTML = `
-            
-    <div class="flex gap-3 items-center">
-      <div class="bg-[#F4F5F7] p-3 rounded-full">
-        <img src="assets/purse1.png" alt="" class="w-10" />
-      </div>
-      <div>
-        <h3 class="font-bold text-lg text-[#525252]">Pay Bill</h3>
-        <p class="text-[#525252] font-semibold">${currentTime}</p>
-      </div>
-    </div>
-    <p class='text-[#525252] font-semibold'>You pay $${amount} to this ${billerNumber} account number of ${selectedBank} (bank or online banking app)</p>
-  
-        `;
-            container.appendChild(div);
-          } else if (amount === 0) {
-            alert("you can`t pay $0 bill");
+              const container = document.getElementById(
+                "transection-container"
+              );
+              const div = document.createElement("div");
+              div.classList.add(
+                "p-6",
+                "bg-white",
+                "mb-4",
+                "rounded-xl",
+                "border",
+                "border-gray-300"
+              );
+              div.innerHTML = `
+      
+<div class="flex gap-3 items-center">
+<div class="bg-[#F4F5F7] p-3 rounded-full">
+  <img src="assets/purse1.png" alt="" class="w-10" />
+</div>
+<div>
+  <h3 class="font-bold text-lg text-[#525252]">Pay Bill</h3>
+  <p class="text-[#525252] font-semibold">${currentTime}</p>
+</div>
+</div>
+<p class='text-[#525252] font-semibold'>You pay $${amount} to this ${billerNumber} account number of ${selectedBank} (bank or online banking app)</p>
+
+  `;
+              container.appendChild(div);
+            } else if (amount === 0) {
+              alert("you can`t pay $0 bill");
+            }
           } else {
-            alert("you can`t pay negative amount bill");
+            alert("you can`t have enough balance");
           }
         } else {
-          alert("you can`t have enough balance");
+          alert("Select a bank");
         }
       } else {
         alert("pin thik nai");
