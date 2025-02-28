646fcaa7a2c4ffe66b3a9456480f228c945b5a86
sarfarazakram576 <sarfaraz.akram055@gmail.com>
2025-02-28 12:04:10 +0600

    Make A fully functional Payoo MFS Banking App

--- script/utilities/utilities.js
+++ script/utilities/utilities.js
@@ -23,3 +23,14 @@ function handleToggle(id, status) {
 document.getElementById("logout-btn").addEventListener("click", function () {
   window.location.href = "index.html";
 });
+
+
+
+// for current time 
+const now = new Date();
+let hours = now.getHours();
+const minutes = now.getMinutes();
+const seconds = now.getSeconds();
+const ampm = hours >= 12 ? "PM" : "AM";
+hours = hours % 12 || 12;
+const currentTime = `${hours}:${minutes}:${seconds} ${ampm}`;
