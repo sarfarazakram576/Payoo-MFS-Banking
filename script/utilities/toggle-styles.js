39ba5bcf4b023a773758998566475f69eaa56f8e
sarfarazakram576 <sarfaraz.akram055@gmail.com>
2025-02-28 19:55:54 +0600

    Make A fully functional Payoo MFS Banking App

--- script/utilities/toggle-styles.js
+++ script/utilities/toggle-styles.js
@@ -1,54 +1,54 @@
 //  for add money
 document.getElementById("add-money").addEventListener("click", function () {
   handleToggleColorAndBcStyles("add-money", "#0874F2");
-  handleToggleBgStyles("add-money-h3", "#F3F8FE");
+  handleToggleBgStyles("add-money", "#F3F8FE");
   handleToggleFwStyles("add-money-h3", "bold");
 
   handleToggleColorAndBcStyles("cashout", "");
-  handleToggleBgStyles("cashout-h3", "");
+  handleToggleBgStyles("cashout", "");
   handleToggleFwStyles("cashout-h3", "");
 
   handleToggleColorAndBcStyles("transfer-money", "");
-  handleToggleBgStyles("transfer-money-h3", "");
+  handleToggleBgStyles("transfer-money", "");
   handleToggleFwStyles("transfer-money-h3", "");
 
   handleToggleColorAndBcStyles("get-bonus", "");
-  handleToggleBgStyles("get-bonus-h3", "");
+  handleToggleBgStyles("get-bonus", "");
   handleToggleFwStyles("get-bonus-h3", "");
 
   handleToggleColorAndBcStyles("pay-bill", "");
-  handleToggleBgStyles("pay-bill-h3", "");
+  handleToggleBgStyles("pay-bill", "");
   handleToggleFwStyles("pay-bill-h3", "");
 
   handleToggleColorAndBcStyles("transactions", "");
-  handleToggleBgStyles("transactions-h3", "");
+  handleToggleBgStyles("transactions", "");
   handleToggleFwStyles("transactions-h3", "");
 });
 
 //  for cashout
 document.getElementById("cashout").addEventListener("click", function () {
   handleToggleColorAndBcStyles("add-money", "");
-  handleToggleBgStyles("add-money-h3", "");
+  handleToggleBgStyles("add-money", "");
   handleToggleFwStyles("add-money-h3", "");
 
   handleToggleColorAndBcStyles("cashout", "#0874F2");
-  handleToggleBgStyles("cashout-h3", "#F3F8FE");
+  handleToggleBgStyles("cashout", "#F3F8FE");
   handleToggleFwStyles("cashout-h3", "bold");
 
   handleToggleColorAndBcStyles("transfer-money", "");
-  handleToggleBgStyles("transfer-money-h3", "");
+  handleToggleBgStyles("transfer-money", "");
   handleToggleFwStyles("transfer-money-h3", "");
 
   handleToggleColorAndBcStyles("get-bonus", "");
-  handleToggleBgStyles("get-bonus-h3", "");
+  handleToggleBgStyles("get-bonus", "");
   handleToggleFwStyles("get-bonus-h3", "");
 
   handleToggleColorAndBcStyles("pay-bill", "");
-  handleToggleBgStyles("pay-bill-h3", "");
+  handleToggleBgStyles("pay-bill", "");
   handleToggleFwStyles("pay-bill-h3", "");
 
   handleToggleColorAndBcStyles("transactions", "");
-  handleToggleBgStyles("transactions-h3", "");
+  handleToggleBgStyles("transactions", "");
   handleToggleFwStyles("transactions-h3", "");
 });
 
@@ -57,108 +57,108 @@ document
   .getElementById("transfer-money")
   .addEventListener("click", function () {
     handleToggleColorAndBcStyles("add-money", "");
-    handleToggleBgStyles("add-money-h3", "");
+    handleToggleBgStyles("add-money", "");
     handleToggleFwStyles("add-money-h3", "");
 
     handleToggleColorAndBcStyles("cashout", "");
-    handleToggleBgStyles("cashout-h3", "");
+    handleToggleBgStyles("cashout", "");
     handleToggleFwStyles("cashout-h3", "");
 
     handleToggleColorAndBcStyles("transfer-money", "#0874F2");
-    handleToggleBgStyles("transfer-money-h3", "#F3F8FE");
+    handleToggleBgStyles("transfer-money", "#F3F8FE");
     handleToggleFwStyles("transfer-money-h3", "bold");
 
     handleToggleColorAndBcStyles("get-bonus", "");
-    handleToggleBgStyles("get-bonus-h3", "");
+    handleToggleBgStyles("get-bonus", "");
     handleToggleFwStyles("get-bonus-h3", "");
 
     handleToggleColorAndBcStyles("pay-bill", "");
-    handleToggleBgStyles("pay-bill-h3", "");
+    handleToggleBgStyles("pay-bill", "");
     handleToggleFwStyles("pay-bill-h3", "");
 
     handleToggleColorAndBcStyles("transactions", "");
-    handleToggleBgStyles("transactions-h3", "");
+    handleToggleBgStyles("transactions", "");
     handleToggleFwStyles("transactions-h3", "");
   });
 
 //  for get bonus
 document.getElementById("get-bonus").addEventListener("click", function () {
   handleToggleColorAndBcStyles("add-money", "");
-  handleToggleBgStyles("add-money-h3", "");
+  handleToggleBgStyles("add-money", "");
   handleToggleFwStyles("add-money-h3", "");
 
   handleToggleColorAndBcStyles("cashout", "");
-  handleToggleBgStyles("cashout-h3", "");
+  handleToggleBgStyles("cashout", "");
   handleToggleFwStyles("cashout-h3", "");
 
   handleToggleColorAndBcStyles("transfer-money", "");
-  handleToggleBgStyles("transfer-money-h3", "");
+  handleToggleBgStyles("transfer-money", "");
   handleToggleFwStyles("transfer-money-h3", "");
 
   handleToggleColorAndBcStyles("get-bonus", "#0874F2");
-  handleToggleBgStyles("get-bonus-h3", "#F3F8FE");
+  handleToggleBgStyles("get-bonus", "#F3F8FE");
   handleToggleFwStyles("get-bonus-h3", "bold");
 
   handleToggleColorAndBcStyles("pay-bill", "");
-  handleToggleBgStyles("pay-bill-h3", "");
+  handleToggleBgStyles("pay-bill", "");
   handleToggleFwStyles("pay-bill-h3", "");
 
   handleToggleColorAndBcStyles("transactions", "");
-  handleToggleBgStyles("transactions-h3", "");
+  handleToggleBgStyles("transactions", "");
   handleToggleFwStyles("transactions-h3", "");
 });
 
 //  for pay bill
 document.getElementById("pay-bill").addEventListener("click", function () {
   handleToggleColorAndBcStyles("add-money", "");
-  handleToggleBgStyles("add-money-h3", "");
+  handleToggleBgStyles("add-money", "");
   handleToggleFwStyles("add-money-h3", "");
 
   handleToggleColorAndBcStyles("cashout", "");
-  handleToggleBgStyles("cashout-h3", "");
+  handleToggleBgStyles("cashout", "");
   handleToggleFwStyles("cashout-h3", "");
 
   handleToggleColorAndBcStyles("transfer-money", "");
-  handleToggleBgStyles("transfer-money-h3", "");
+  handleToggleBgStyles("transfer-money", "");
   handleToggleFwStyles("transfer-money-h3", "");
 
   handleToggleColorAndBcStyles("get-bonus", "");
-  handleToggleBgStyles("get-bonus-h3", "");
+  handleToggleBgStyles("get-bonus", "");
   handleToggleFwStyles("get-bonus-h3", "");
 
   handleToggleColorAndBcStyles("pay-bill", "#0874F2");
-  handleToggleBgStyles("pay-bill-h3", "#F3F8FE");
+  handleToggleBgStyles("pay-bill", "#F3F8FE");
   handleToggleFwStyles("pay-bill-h3", "bold");
 
   handleToggleColorAndBcStyles("transactions", "");
-  handleToggleBgStyles("transactions-h3", "");
+  handleToggleBgStyles("transactions", "");
   handleToggleFwStyles("transactions-h3", "");
 });
 
 //  for transactions
 document.getElementById("transactions").addEventListener("click", function () {
   handleToggleColorAndBcStyles("add-money", "");
-  handleToggleBgStyles("add-money-h3", "");
+  handleToggleBgStyles("add-money", "");
   handleToggleFwStyles("add-money-h3", "");
 
   handleToggleColorAndBcStyles("cashout", "");
-  handleToggleBgStyles("cashout-h3", "");
+  handleToggleBgStyles("cashout", "");
   handleToggleFwStyles("cashout-h3", "");
 
   handleToggleColorAndBcStyles("transfer-money", "");
-  handleToggleBgStyles("transfer-money-h3", "");
+  handleToggleBgStyles("transfer-money", "");
   handleToggleFwStyles("transfer-money-h3", "");
 
   handleToggleColorAndBcStyles("get-bonus", "");
-  handleToggleBgStyles("get-bonus-h3", "");
+  handleToggleBgStyles("get-bonus", "");
   handleToggleFwStyles("get-bonus-h3", "");
 
   handleToggleColorAndBcStyles("pay-bill", "");
-  handleToggleBgStyles("pay-bill-h3", "");
+  handleToggleBgStyles("pay-bill", "");
   handleToggleFwStyles("pay-bill-h3", "");
 
   handleToggleColorAndBcStyles("transactions", "#0874F2");
-  handleToggleBgStyles("transactions-h3", "#F3F8FE");
+  handleToggleBgStyles("transactions", "#F3F8FE");
   handleToggleFwStyles("transactions-h3", "bold");
 });
 
