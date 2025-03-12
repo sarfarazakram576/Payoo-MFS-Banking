fca4ec7005f32f867fe2855bdfca27b2c5fbbdbe
sarfarazakram576 <sarfaraz.akram055@gmail.com>
2025-03-12 19:21:50 +0600

    complete payoo mfs banking website

--- script/getBonus.js
+++ script/getBonus.js
@@ -2,9 +2,13 @@ document.getElementById("get-bonus-btn").addEventListener("click", function () {
   const value = document.getElementById("coupon").value;
   const mainBalance = getInnerTextById("mainBalance");
 
-  if (value === "GetBonus100") {
+  if (!value) {
+    alert("give coupon");
+  } else if (value === "GetBonus100") {
     const newMainBalance = mainBalance + 100;
     setInnerTextByIdAndValue("mainBalance", newMainBalance);
+
+    const uniqueID = `trans_${new Date().getTime()}`;
 
     const container = document.getElementById("transection-container");
     const div = document.createElement("div");
@@ -17,7 +21,7 @@ document.getElementById("get-bonus-btn").addEventListener("click", function () {
       "border-gray-300"
     );
     div.innerHTML = `
-          <div id="details4">      
+               
         <div class="flex gap-3 items-center justify-between">
         <div class='flex items-center gap-3'>
           <div class="bg-[#F4F5F7] p-3 rounded-full">
@@ -28,43 +32,36 @@ document.getElementById("get-bonus-btn").addEventListener("click", function () {
             <p class="text-[#525252] font-semibold">${currentTime}</p>
           </div>
           </div>
-          <div class=''>
-          <i id='showDetails4' class="cursor-pointer fa-solid fa-ellipsis-vertical text-[#525252] text-xl"></i>
-         <i id='hideDetails4'  class="cursor-pointer fa-solid fa-xmark text-[#525252] text-xl"></i>
+          <div>
+          <i id='${uniqueID}_toggle' class="cursor-pointer fa-solid fa-ellipsis-vertical text-[#525252] text-xl"></i>
+        
           </div>
           </div>
-          </div>
+            <p id='${uniqueID}_details' class='hidden text-[#525252]  font-semibold'>You transfer $100 as a bonus from us</p>
+
           `;
     container.appendChild(div);
 
-    document.getElementById("hideDetails4").style.display = "none";
+    const transactionDetails = document.getElementById(`${uniqueID}_details`);
 
-    document
-      .getElementById("showDetails4")
-      .addEventListener("click", function () {
-        const p = document.createElement("p");
-        p.classList.add("text-[#525252]", "font-semibold");
-        p.innerText = `You transfer $100 as a bonus from us`;
-
-        const details4 = document.getElementById("details4");
-        details4.appendChild(p);
-
-        document.getElementById("showDetails4").style.display = "none";
-        document.getElementById("hideDetails4").style.display = "block";
-
-        document
-          .getElementById("hideDetails4")
-          .addEventListener("click", function () {
-            document.getElementById("showDetails4").style.display = "block";
-            document.getElementById("hideDetails4").style.display = "none";
-
-            details4.removeChild(p);
-          });
-      });
+    const toggleDetails = document.getElementById(`${uniqueID}_toggle`);
+    toggleDetails.addEventListener("click", () => {
+      if (transactionDetails.classList.contains("hidden")) {
+        transactionDetails.classList.remove("hidden");
+        toggleDetails.classList.remove("fa-ellipsis-vertical");
+        toggleDetails.classList.add("fa-xmark");
+      } else {
+        transactionDetails.classList.add("hidden");
+        toggleDetails.classList.add("fa-ellipsis-vertical");
+        toggleDetails.classList.remove("fa-xmark");
+      }
+    });
   } else if (value === "GetBonus300") {
     const newMainBalance = mainBalance + 300;
     setInnerTextByIdAndValue("mainBalance", newMainBalance);
 
+    const uniqueID = `trans_${new Date().getTime()}`;
+
     const container = document.getElementById("transection-container");
     const div = document.createElement("div");
     div.classList.add(
@@ -76,7 +73,7 @@ document.getElementById("get-bonus-btn").addEventListener("click", function () {
       "border-gray-300"
     );
     div.innerHTML = `
-          <div id="details5">      
+            
         <div class="flex gap-3 items-center justify-between">
         <div class='flex items-center gap-3'>
           <div class="bg-[#F4F5F7] p-3 rounded-full">
@@ -87,42 +84,35 @@ document.getElementById("get-bonus-btn").addEventListener("click", function () {
             <p class="text-[#525252] font-semibold">${currentTime}</p>
           </div>
           </div>
-          <div class=''>
-          <i id='showDetails5' class="cursor-pointer fa-solid fa-ellipsis-vertical text-[#525252] text-xl"></i>
-         <i id='hideDetails5'  class="cursor-pointer fa-solid fa-xmark text-[#525252] text-xl"></i>
+          <div>
+          <i id='${uniqueID}_toggle' class="cursor-pointer fa-solid fa-ellipsis-vertical text-[#525252] text-xl"></i>
+        
           </div>
           </div>
-          </div>
+            <p id='${uniqueID}_details' class='hidden text-[#525252]  font-semibold'>You transfer $300 as a bonus from us</p>
+
           `;
     container.appendChild(div);
 
-    document.getElementById("hideDetails5").style.display = "none";
+    const transactionDetails = document.getElementById(`${uniqueID}_details`);
 
-    document
-      .getElementById("showDetails5")
-      .addEventListener("click", function () {
-        const p = document.createElement("p");
-        p.classList.add("text-[#525252]", "font-semibold");
-        p.innerText = `You transfer $300 as a bonus from us`;
-
-        const details5 = document.getElementById("details5");
-        details5.appendChild(p);
-
-        document.getElementById("showDetails5").style.display = "none";
-        document.getElementById("hideDetails5").style.display = "block";
-
-        document
-          .getElementById("hideDetails5")
-          .addEventListener("click", function () {
-            document.getElementById("showDetails5").style.display = "block";
-            document.getElementById("hideDetails5").style.display = "none";
-
-            details5.removeChild(p);
-          });
-      });
+    const toggleDetails = document.getElementById(`${uniqueID}_toggle`);
+    toggleDetails.addEventListener("click", () => {
+      if (transactionDetails.classList.contains("hidden")) {
+        transactionDetails.classList.remove("hidden");
+        toggleDetails.classList.remove("fa-ellipsis-vertical");
+        toggleDetails.classList.add("fa-xmark");
+      } else {
+        transactionDetails.classList.add("hidden");
+        toggleDetails.classList.add("fa-ellipsis-vertical");
+        toggleDetails.classList.remove("fa-xmark");
+      }
+    });
   } else if (value === "GetBonus500") {
     const newMainBalance = mainBalance + 500;
     setInnerTextByIdAndValue("mainBalance", newMainBalance);
+
+    const uniqueID = `trans_${new Date().getTime()}`;
 
     const container = document.getElementById("transection-container");
     const div = document.createElement("div");
@@ -146,42 +136,35 @@ document.getElementById("get-bonus-btn").addEventListener("click", function () {
             <p class="text-[#525252] font-semibold">${currentTime}</p>
           </div>
           </div>
-          <div class=''>
-          <i id='showDetails6' class="cursor-pointer fa-solid fa-ellipsis-vertical text-[#525252] text-xl"></i>
-         <i id='hideDetails6'  class="cursor-pointer fa-solid fa-xmark text-[#525252] text-xl"></i>
+          <div>
+          <i id='${uniqueID}_toggle' class="cursor-pointer fa-solid fa-ellipsis-vertical text-[#525252] text-xl"></i>
+        
           </div>
           </div>
-          </div>
+            <p id='${uniqueID}_details' class='hidden text-[#525252]  font-semibold'>You transfer $500 as a bonus from us</p>
+
           `;
     container.appendChild(div);
 
-    document.getElementById("hideDetails6").style.display = "none";
+    const transactionDetails = document.getElementById(`${uniqueID}_details`);
 
-    document
-      .getElementById("showDetails6")
-      .addEventListener("click", function () {
-        const p = document.createElement("p");
-        p.classList.add("text-[#525252]", "font-semibold");
-        p.innerText = `You transfer $500 as a bonus from us`;
-
-        const details6 = document.getElementById("details6");
-        details6.appendChild(p);
-
-        document.getElementById("showDetails6").style.display = "none";
-        document.getElementById("hideDetails6").style.display = "block";
-
-        document
-          .getElementById("hideDetails6")
-          .addEventListener("click", function () {
-            document.getElementById("showDetails6").style.display = "block";
-            document.getElementById("hideDetails6").style.display = "none";
-
-            details6.removeChild(p);
-          });
-      });
+    const toggleDetails = document.getElementById(`${uniqueID}_toggle`);
+    toggleDetails.addEventListener("click", () => {
+      if (transactionDetails.classList.contains("hidden")) {
+        transactionDetails.classList.remove("hidden");
+        toggleDetails.classList.remove("fa-ellipsis-vertical");
+        toggleDetails.classList.add("fa-xmark");
+      } else {
+        transactionDetails.classList.add("hidden");
+        toggleDetails.classList.add("fa-ellipsis-vertical");
+        toggleDetails.classList.remove("fa-xmark");
+      }
+    });
   } else if (value === "GetBonus1000") {
     const newMainBalance = mainBalance + 1000;
     setInnerTextByIdAndValue("mainBalance", newMainBalance);
+
+    const uniqueID = `trans_${new Date().getTime()}`;
 
     const container = document.getElementById("transection-container");
     const div = document.createElement("div");
@@ -205,39 +188,30 @@ document.getElementById("get-bonus-btn").addEventListener("click", function () {
             <p class="text-[#525252] font-semibold">${currentTime}</p>
           </div>
           </div>
-          <div class=''>
-          <i id='showDetails7' class="cursor-pointer fa-solid fa-ellipsis-vertical text-[#525252] text-xl"></i>
-         <i id='hideDetails7'  class="cursor-pointer fa-solid fa-xmark text-[#525252] text-xl"></i>
+          <div>
+          <i id='${uniqueID}_toggle' class="cursor-pointer fa-solid fa-ellipsis-vertical text-[#525252] text-xl"></i>
+        
           </div>
           </div>
-          </div>
+            <p id='${uniqueID}_details' class='hidden text-[#525252]  font-semibold'>You transfer $1000 as a bonus from us</p>
+
           `;
     container.appendChild(div);
 
-    document.getElementById("hideDetails7").style.display = "none";
+    const transactionDetails = document.getElementById(`${uniqueID}_details`);
 
-    document
-      .getElementById("showDetails7")
-      .addEventListener("click", function () {
-        const p = document.createElement("p");
-        p.classList.add("text-[#525252]", "font-semibold");
-        p.innerText = `You transfer $1000 as a bonus from us`;
-
-        const details7 = document.getElementById("details7");
-        details7.appendChild(p);
-
-        document.getElementById("showDetails7").style.display = "none";
-        document.getElementById("hideDetails7").style.display = "block";
-
-        document
-          .getElementById("hideDetails7")
-          .addEventListener("click", function () {
-            document.getElementById("showDetails7").style.display = "block";
-            document.getElementById("hideDetails7").style.display = "none";
-
-            details7.removeChild(p);
-          });
-      });
+    const toggleDetails = document.getElementById(`${uniqueID}_toggle`);
+    toggleDetails.addEventListener("click", () => {
+      if (transactionDetails.classList.contains("hidden")) {
+        transactionDetails.classList.remove("hidden");
+        toggleDetails.classList.remove("fa-ellipsis-vertical");
+        toggleDetails.classList.add("fa-xmark");
+      } else {
+        transactionDetails.classList.add("hidden");
+        toggleDetails.classList.add("fa-ellipsis-vertical");
+        toggleDetails.classList.remove("fa-xmark");
+      }
+    });
   } else {
     alert("The given coupon is valid for now");
   }
